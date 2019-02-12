cea60e41acaa3efade05de992c452d0c86154d38
PeaceOfGod <42493347+PeaceOfGod@users.noreply.github.com>
2019-02-12 09:41:16 +0100

    Adding Comments to code sections

--- sockMerchant.java
+++ sockMerchant.java
@@ -26,7 +26,8 @@ public class Solution {
         }
     return count;
     }
-
+    
+    //Private static Method
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
@@ -34,17 +35,21 @@ public class Solution {
 
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
+        
+        //Initializing and Creating of an Array of n INTEGERS
         int[] ar = new int[n];
 
+        //Passing a function to split into new lines strings that would be entered
         String[] arItems = scanner.nextLine().split(" ");
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
+        //Adding the each elements in arItem to ar after casting the string to integer 
         for (int i = 0; i < n; i++) {
             int arItem = Integer.parseInt(arItems[i]);
             ar[i] = arItem;
         }
 
+        //Calling the sockMerchant method
         int result = sockMerchant(n, ar);
 
         bufferedWriter.write(String.valueOf(result));
